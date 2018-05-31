815dd987ab555c00078f14700b64a09bd5ea6706
liudaoqiang <liu18801799437@163.com>
2018-05-31 23:48:16 +0800

    装饰器
    带参数的装饰器函数
    用functools.wrap()f)传递f的属性到wrapper函数中

--- python进阶.py
+++ python进阶.py
@@ -62,18 +62,75 @@ def count():
 f1, f2, f3 = count()#此时f1,f2,f3返回的是
 print(f1(),f2(),f3())#1,4,9
 
-#无参数的装饰器函数
+#无参数参数的装饰器函数
 import time
 def log(f):
 	def fn(*args,**kwargs):
-		print('call'+f.__name__()+ctime())
 		t1 = time.clock()
 		r = f(*args,**kwargs)
 		t2 = time.clock()
-		print('call time:%d' %(t2-t1))
+		print('call %s call time:%d' %(f.__name__(),t2-t1))
 		return r
 	return fn
 @log
 def factorial(n):
 	return reduce(lambda x,y:x+y,range(1, n))
 print(factorial(6))
+
+#装饰器 封装性，不改变原有代码新增功能，
+#缺点：装饰器返回的函数已经不是原有的函数，所以依赖原有函数名的代码会失效，而且装饰器还改变了原有函数的__doc__属性
+#如果要让调用者看不出一个函数经过装饰器的”改造“，就需要把原函数的一些属性复制到新函数中，functools
+import functools
+def log(f):
+	@functools.wraps(f)
+	def wrapper(*args,**kwargs):
+		print('call ...')
+		return f(*args,**kwargs)
+	return wrapper
+@log
+def f2(x):
+	pass
+print(f2.__name__)#将会打印f2
+
+#如果没有用functools来复制
+import functools
+def log(f):
+	#@functools.wraps(f)
+	def wrapper(*args,**kwargs):
+		print('call ...')
+		return f(*args,**kwargs)
+	return wrapper
+@log
+def f2(x):
+	pass
+print(f2.__name__)#将会打印wrapper,依赖f2函数名的代码会失效，而且wrapper不会有f2的属性
+
+#带参数的装饰器函数
+#实现带参数的装饰器函数就需要三层嵌套的函数
+import time,functools
+def performance(unit):
+	def perf_decorator(f):
+		@functools.wraps(f)
+		def wrapper(*args,**kwargs):
+			t1 = time.time()
+			r = f(*args,**kwargs)
+			t2 = time.time()
+			t = (t2 - t1)*1000 if unit=='ms' else (t2-t1)
+			print('call %s() in %f %s' %(f.__name__,t,unit))
+			return r
+		return wrapper
+	return perf_decorator
+
+@performance('ms')
+def factorial(n):
+	return reduce(lambda x,y:x*y,range(1,n+1))
+print(factorial(10))
+#返回结果
+#call factorial() in 0.000000 ms
+# 3628800
+
+#偏函数
+import functools
+int2 = funtools.partial(int,base=2)
+int2('1000000')
+#返回64
